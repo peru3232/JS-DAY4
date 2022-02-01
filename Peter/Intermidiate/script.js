2b272cc49590008f929c3be9af6a052cb41c11d6
peru3232 <prw@qmail.com.de>
2022-02-01 14:17:59 +0100

    intermediate done

--- Peter/Intermidiate/script.js
+++ Peter/Intermidiate/script.js
@@ -1,13 +1,31 @@
 // Exercise 1
 const matrixArray = document.getElementsByName('santa');
 
-function diappearItem() {
+function disappearItem() {
     // console.log(this)
+    if (this.style.opacity == "1") {
     this.style.opacity= "0";
     this.style.transition = "500ms";
+    } else {this.style.opacity= "1"}
 }
 
-for (let index = 0; index < matrixArray.length; index +=1) {
-    matrixArray[index].addEventListener('click', diappearItem)
+for (let index = 0; index < matrixArray.length; index ++) {
+    matrixArray[index].addEventListener('click', disappearItem);
+    matrixArray[index].style.opacity = "1";
 }
 
+
+//Exercise 2
+const button = document.getElementById('button');
+const main = document.getElementsByTagName('main');
+
+function randomColorValue() {
+    return Math.round((Math.random() * 255));
+}
+
+function changeBackground() {
+    const value = `rgb(${randomColorValue()}, ${randomColorValue()}, ${randomColorValue()})`;
+    main[0].style.background = value;
+}
+
+button.addEventListener("click", changeBackground);
